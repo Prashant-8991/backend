d0c761f749e3731ab58d808632e0cb77df9352eb
brijesh <brijesh@gmail.com>
2026-05-27 12:53:20 +0530

    redis added for caching and improve a speed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Depends, HTTPException
 from app.config.db import get_session
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_session
 from sqlalchemy import text
 from app.schemas.dashboardschema import CattleDashboardApiResponse
 from app.schemas.cattleprofileschema import CattleProfileApiResponse
@@ -12,6 +12,8 @@ from app.schemas.donatedoutschema import DonatedOutRecord
 from app.schemas.cattlecardschema import CattleCardResponse
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
+import redis
+import json
 
 load_dotenv()
 
@@ -28,16 +30,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+r = redis.Redis(
+    host="localhost", port=6379, password="demo@12341234", decode_responses=True
+)
+
 
 @app.get("/dashboard", response_model=CattleDashboardApiResponse)
-async def get_dashboard(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(text("SELECT get_dashboard_data()"))
-    dashboard_data = result.scalar()
+async def get_dashboard():
 
-    if dashboard_data is None:
-        raise HTTPException(status_code=404, detail="Dashboard data not found")
+    # Try Redis first
+    dashboard_data_json = r.get("dashboard_data")
 
-    return dashboard_data
+    if dashboard_data_json:
+        return json.loads(dashboard_data_json)
+
+    async for session in get_session():
+
+        result = await session.execute(text("SELECT get_dashboard_data()"))
+
+        dashboard_data = result.scalar()
+
+        if dashboard_data is None:
+            raise HTTPException(status_code=404, detail="Dashboard data not found")
+
+        # Store in Redis
+        r.setex("dashboard_data", 20, json.dumps(dashboard_data))  # 1 hour
+
+        return dashboard_data
 
 
 @app.get("/cattle/{tag_number}", response_model=CattleProfileApiResponse)
@@ -59,12 +78,17 @@ async def get_cattle_profile(
 
 
 @app.get("/all-present-cattle", response_model=List[PresentCattleModel])
-async def all_present_cattle(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(text("SELECT * FROM get_all_present_cattle();"))
-    print(result)
-
-    cattle_list = result.scalar_one_or_none()
-    return cattle_list
+async def all_present_cattle():
+    all_present_cattle_json = r.get("all_present_cattle")
+    if all_present_cattle_json:
+        return json.loads(all_present_cattle_json)
+    async for session in get_session():
+        result = await session.execute(text("SELECT * FROM get_all_present_cattle();"))
+        cattle_list = result.scalar_one_or_none()
+        if cattle_list is None:
+            raise HTTPException(status_code=404, detail="All Present Cattle Data not avaialable error")
+        r.setex("all_present_cattle", 30, json.dumps(cattle_list))
+        return cattle_list
 
 
 @app.get("/all-milking-cattle", response_model=List[PresentCattleModel])
@@ -75,19 +99,31 @@ async def get_milking_cattle(session: AsyncSession = Depends(get_session)):
 
 
 @app.get("/genealogy/all", response_model=List[GenealogyCattle])
-async def get_genealogy_all(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(
-        text("SELECT * FROM get_all_cattle_for_genealogy();")
-    )
-    cattle_list = result.scalar_one_or_none()
-    return cattle_list or []
+async def get_genealogy_all():
+    genealogy_data_json = r.get("all_genealogy")
+    if genealogy_data_json:
+        return json.loads(genealogy_data_json)
+    async for session in get_session():
+        result = await session.execute(
+            text("SELECT * FROM get_all_cattle_for_genealogy();")
+        )
+        cattle_list = result.scalar_one_or_none()
+        if cattle_list is None:
+            raise HTTPException(status_code=404, detail="Geneology data missing error")
+        r.setex("all_genealogy", 3600 * 24, json.dumps(cattle_list))
+        return cattle_list or []
 
 
 @app.get("/donations/donated-out", response_model=List[DonatedOutRecord])
 async def get_donated_out(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(text("SELECT get_donated_out_cattle()"))
-    records = result.scalar_one_or_none()
-    return records or []
+    donated_out_json = r.get("donated_out")
+    if donated_out_json:
+        return json.loads(donated_out_json)
+    async for session in get_session():
+        result = await session.execute(text("SELECT get_donated_out_cattle()"))
+        records = result.scalar_one_or_none()
+        r.setex("donated_out", 3600 * 24 * 365, json.dumps(records))
+        return records or []
 
 
 @app.get("/cattle-card/{tag_number}", response_model=CattleCardResponse)
